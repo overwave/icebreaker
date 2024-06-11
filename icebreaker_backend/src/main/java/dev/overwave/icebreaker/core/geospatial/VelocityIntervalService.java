75e28fc5be9ecf248f7c024bc2bdb748cfb44628
Lisa Gavriushkina <lisa@infotec.ru>
2024-06-11 17:42:31 +0300

    метод resetIntegralVelocities теперь записывает новые данные в файл

--- icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/geospatial/VelocityIntervalService.java
+++ icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/geospatial/VelocityIntervalService.java
@@ -2,6 +2,7 @@ package dev.overwave.icebreaker.core.geospatial;
 
 import dev.overwave.icebreaker.api.navigation.VelocityIntervalDto;
 import dev.overwave.icebreaker.core.parser.XlsxParser;
+import dev.overwave.icebreaker.core.serialization.SerializationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,10 +24,10 @@ public class VelocityIntervalService {
     }
 
     public void resetIntegralVelocities(InputStream inputStream) {
-        //TODO
+        // парсим данные о ледовой проходимости
         List<List<RawVelocity>> unsavedRawVelocities = XlsxParser.parseIntegralVelocityTable(inputStream);
         RawVelocity first = unsavedRawVelocities.getFirst().getFirst();
-
+        // получаем список всех новых интервалов и обновляем данные в бд
         List<VelocityInterval> unsavedVelocityIntervals = new ArrayList<>();
         for (ContinuousVelocity continuousVelocity : first.velocities()) {
             Interval interval = continuousVelocity.interval();
@@ -38,5 +39,8 @@ public class VelocityIntervalService {
         }
         velocityIntervalRepository.deleteAll();
         List<VelocityInterval> intervals = velocityIntervalRepository.saveAllAndFlush(unsavedVelocityIntervals);
+        // пишем в файл новые данные о ледовой проходимости в виде сетки
+        List<SpatialVelocity> spatialVelocities = SpatialVelocityFactory.formSpatialVelocityGrid(unsavedRawVelocities);
+        SerializationUtils.writeSpatial(spatialVelocities, "data/spatial_velocities.dat");
     }
 }
