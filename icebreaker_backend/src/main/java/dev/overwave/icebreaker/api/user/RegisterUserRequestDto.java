8cae94d8c4578ff8cdb8301171150d04287ad1c0
Rodion Kabanov <overwave2@gmail.com>
2024-06-12 19:51:44 +0300

    added role to registration dto

--- icebreaker_backend/src/main/java/dev/overwave/icebreaker/api/user/RegisterUserRequestDto.java
+++ icebreaker_backend/src/main/java/dev/overwave/icebreaker/api/user/RegisterUserRequestDto.java
@@ -1,11 +1,14 @@
 package dev.overwave.icebreaker.api.user;
 
+import dev.overwave.icebreaker.core.user.UserRole;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterUserRequestDto(
         @NotBlank
         String login,
         @NotBlank
-        String password
+        String password,
+        @NotBlank
+        UserRole role
 ) {
 }
