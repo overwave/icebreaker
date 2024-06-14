59a6c17c161fcda6a00282f9ef1af51a3bfffc1a
Rodion Kabanov <overwave2@gmail.com>
2024-06-14 17:58:42 +0300

    icebreaker assignment start

--- icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/schedule/ScheduledShip.java
+++ icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/schedule/ScheduledShip.java
@@ -2,12 +2,14 @@ package dev.overwave.icebreaker.core.schedule;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 import java.time.Instant;
 import java.util.List;
 
 @Data
 @Builder
+@Accessors(chain = true)
 public class ScheduledShip {
     private long shipId;
     private long currentNavigationPointId;
