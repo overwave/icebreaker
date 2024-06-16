2ae758ba1776901d1a884811646893f7b72d3780
Rodion Kabanov <overwave2@gmail.com>
2024-06-16 12:59:08 +0300

    returning navigation request for ship

--- icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/navigation/NavigationRequestService.java
+++ icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/navigation/NavigationRequestService.java
@@ -1,23 +1,34 @@
 package dev.overwave.icebreaker.core.navigation;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.overwave.icebreaker.api.navigation.NavigationRequestDto;
 import dev.overwave.icebreaker.api.navigation.NavigationRequestPendingDto;
 import dev.overwave.icebreaker.api.navigation.NavigationRequestToSaveDto;
 import dev.overwave.icebreaker.api.navigation.NavigationRequestWithRouteDto;
 import dev.overwave.icebreaker.api.navigation.NavigationRequestsDtoWithRoute;
+import dev.overwave.icebreaker.api.navigation.PointAndTimestamp;
 import dev.overwave.icebreaker.api.navigation.RouteSegmentDto;
 import dev.overwave.icebreaker.api.navigation.ShipRouteDto;
-import dev.overwave.icebreaker.core.schedule.ScheduleService;
+import dev.overwave.icebreaker.core.geospatial.Point;
 import dev.overwave.icebreaker.core.schedule.ContextHolder;
+import dev.overwave.icebreaker.core.schedule.ScheduleService;
+import dev.overwave.icebreaker.core.schedule.ShipRouteEntity;
+import dev.overwave.icebreaker.core.schedule.ShipRouteMapper;
+import dev.overwave.icebreaker.core.schedule.ShipRouteRepository;
 import dev.overwave.icebreaker.core.ship.Ship;
 import dev.overwave.icebreaker.core.ship.ShipRepository;
 import dev.overwave.icebreaker.core.user.User;
 import dev.overwave.icebreaker.core.user.UserRepository;
 import dev.overwave.icebreaker.core.user.UserRole;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,9 +39,12 @@ public class NavigationRequestService {
     private final NavigationRequestRepository navigationRequestRepository;
     private final NavigationRequestMapper navigationRequestMapper;
     private final ShipRepository shipRepository;
+    private final ShipRouteRepository shipRouteRepository;
     private final NavigationPointRepository navigationPointRepository;
     private final UserRepository userRepository;
     private final ScheduleService scheduleService;
+    private final ObjectMapper objectMapper;
+    private final ShipRouteMapper shipRouteMapper;
     private final ContextHolder contextHolder;
 
     public void saveNavigationRequest(NavigationRequestToSaveDto requestDto) {
@@ -106,7 +120,54 @@ public class NavigationRequestService {
     }
 
     public List<ShipRouteDto> getShipRouteByRequestId(long navigationRequestId) {
-        NavigationRequest request = navigationRequestRepository.findByIdOrThrow(navigationRequestId);
-        return scheduleService.createPreliminaryShipRoute(request.getShip().getId());
+        List<ShipRouteEntity> routeSegments =
+                shipRouteRepository.findAllByNavigationRequestIdOrderById(navigationRequestId);
+        List<ShipRouteDto> routes = new ArrayList<>();
+        for (ShipRouteEntity routeSegment : routeSegments) {
+            if (isWaiting(routeSegment)) {
+                continue;
+            }
+            List<Long> companions = getCompanions(routeSegment);
+            ShipRouteDto route = ShipRouteDto.builder().id(routeSegment.getId())
+                    .convoy(!companions.isEmpty())
+                    .icebreaker(companions.stream().findFirst().map(String::valueOf).orElse(""))
+                    .routes(getHourlyPoints(routeSegment))
+                    .build();
+            routes.add(route);
+        }
+        return routes;
+    }
+
+    private List<PointAndTimestamp> getHourlyPoints(ShipRouteEntity routeSegment) {
+        if (isWaiting(routeSegment)) {
+            List<PointAndTimestamp> points = new ArrayList<>();
+            for (Instant i = routeSegment.getStartDate().truncatedTo(ChronoUnit.HOURS);
+                 i.isBefore(routeSegment.getEndDate());
+                 i = i.plus(Duration.ofHours(1))) {
+                points.add(new PointAndTimestamp(routeSegment.getStartPoint().getPoint(), i.getEpochSecond()));
+            }
+            return points;
+        } else {
+            List<Point> points = getPoints(routeSegment);
+            return shipRouteMapper.createPointAndTimestamp(routeSegment.getStartDate(), points);
+        }
+    }
+
+    private boolean isWaiting(ShipRouteEntity routeSegment) {
+        return routeSegment.getStartPoint().getPoint().equals(routeSegment.getFinishPoint().getPoint());
+    }
+
+    @SneakyThrows
+    private List<Long> getCompanions(ShipRouteEntity routeSegment) {
+        TypeReference<List<Long>> listLong = new TypeReference<>() {
+        };
+        return objectMapper.readValue(routeSegment.getCompanions(), listLong);
+    }
+
+    @SneakyThrows
+    private List<Point> getPoints(ShipRouteEntity routeSegment) {
+        TypeReference<List<Point>> listPoints = new TypeReference<>() {
+        };
+        return objectMapper.readValue(routeSegment.getPoints(), listPoints);
     }
 }
--- icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/schedule/ShipRouteMapper.java
+++ icebreaker_backend/src/main/java/dev/overwave/icebreaker/core/schedule/ShipRouteMapper.java
@@ -6,6 +6,7 @@ import dev.overwave.icebreaker.core.geospatial.Point;
 import dev.overwave.icebreaker.core.route.Route;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -14,24 +15,20 @@ import java.util.List;
 @Component
 public class ShipRouteMapper {
 
-    public ShipRouteDto toShipRouteDto(long id, RoutePredictionSegment prediction, Route route,
-                                       ScheduledShip icebreaker) {
+    public ShipRouteDto toShipRouteDto(long id, RoutePredictionSegment prediction, Route route) {
         List<PointAndTimestamp> routes = createPointAndTimestamp(route.interval().instant(), route.normalizedPoints());
         return new ShipRouteDto(
                 id,
                 prediction.convoy(),
-                icebreaker.getShipId(),
+                "",
                 routes);
     }
 
-    private List<PointAndTimestamp> createPointAndTimestamp(Instant startDate, List<Point> normalizedPoints) {
+    public List<PointAndTimestamp> createPointAndTimestamp(Instant startDate, List<Point> normalizedPoints) {
         List<PointAndTimestamp> routes = new ArrayList<>();
-        routes.add(new PointAndTimestamp(normalizedPoints.getFirst(), startDate.getEpochSecond()));
-        Instant instant = startDate;
-        for (int i = 1; i < normalizedPoints.size(); i++) {
-            Point point = normalizedPoints.get(i);
-            instant = instant.plus(1, ChronoUnit.HOURS);
-            routes.add(new PointAndTimestamp(point, instant.getEpochSecond()));
+        for (int i = 0; i < normalizedPoints.size(); i++) {
+            long epochSecond = startDate.truncatedTo(ChronoUnit.HOURS).plus(Duration.ofHours(i)).getEpochSecond();
+            routes.add(new PointAndTimestamp(normalizedPoints.get(i), epochSecond));
         }
         return routes;
     }
